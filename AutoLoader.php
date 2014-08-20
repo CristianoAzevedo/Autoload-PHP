1fd565dc0a19a9f9e07429bdd166f5f88f9b26e5
Cristiano <cristianodasilva.azevedo@gmail.com>
2014-08-20 15:21:05 -0300

    funcao autoload

---
+++ AutoLoader.php
@@ -0,0 +1,17 @@
+<?php 
+	function autoload($className) {
+		$diretorios = array(
+			'Model/',
+			'Dao/'
+		); //array dos locais onde estão as classes
+		
+	 foreach ($diretorios as $diretorio){//laço para listar o diretorios
+		set_include_path($diretorio);
+		spl_autoload($className); //funciona com inclue/require
+	 }
+	
+	}	
+	spl_autoload_extensions('.php'); // defino a extensão
+	spl_autoload_register('autoload'); 
+	
+ ?>
